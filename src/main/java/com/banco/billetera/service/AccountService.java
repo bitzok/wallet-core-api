0b46236e259dcf2042102c4305bae6fd20559430
bitzok <santacruzedward@hotmail.com>
2026-07-30 17:26:49 -0500

    feat: implement transfer money business logic and validations

--- src/main/java/com/banco/billetera/service/AccountService.java
+++ src/main/java/com/banco/billetera/service/AccountService.java
@@ -1,8 +1,11 @@
 package com.banco.billetera.service;
 
+import com.banco.billetera.entity.Account;
+import com.banco.billetera.entity.Transaction;
 import com.banco.billetera.repository.AccountRepository;
 import com.banco.billetera.repository.TransactionRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -17,7 +20,30 @@ public class AccountService {
         this.transactionRepository = transactionRepository;
     }
 
+    @Transactional
     public void transferMoney(UUID sourceAccountId, UUID destinationAccountId, BigDecimal amount){
+        Account existSource = accountRepository.findById(sourceAccountId).orElseThrow(() -> new IllegalArgumentException("La cuenta origen no existe"));
+        Account existDestination = accountRepository.findById(destinationAccountId).orElseThrow(() -> new IllegalArgumentException("La cuenta destino no existe"));
 
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("El monto debe ser mayor a cero");
+        }
+
+        if (amount.compareTo(existSource.getBalance()) > 0){
+            throw new IllegalArgumentException("Fondos insuficientes");
+        }
+
+        existSource.setBalance(existSource.getBalance().subtract(amount));
+        existDestination.setBalance(existDestination.getBalance().add(amount));
+
+        accountRepository.save(existSource);
+        accountRepository.save(existDestination);
+
+        Transaction transaccion = new Transaction();
+        transaccion.setAmount(amount);
+        transaccion.setSourceAccountId(sourceAccountId);
+        transaccion.setDestinationAccountId(destinationAccountId);
+
+        transactionRepository.save(transaccion);
     }
 }
