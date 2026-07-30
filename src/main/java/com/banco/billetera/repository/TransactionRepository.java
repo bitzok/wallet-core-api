b47701704bc31c09e0987e4efcbe929efe2684f9
bitzok <santacruzedward@hotmail.com>
2026-07-30 16:17:35 -0500

    feat: add Transaction entity

---
+++ src/main/java/com/banco/billetera/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.banco.billetera.repository;
+
+import com.banco.billetera.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TransactionRepository extends JpaRepository<Transaction, UUID>  {
+}
